90399aba346d8b94cc28e75353e6f1cfbaa30999
DustofTimeYT <71147997+DustofTimeYT@users.noreply.github.com>
2022-01-08 16:17:56 +0300

    1

--- CourseApp/Phone/LandLinePhone.cs
+++ CourseApp/Phone/LandLinePhone.cs
@@ -4,9 +4,25 @@
 
     public class LandLinePhone : Phone, IProduct
     {
+        private double price;
+
         public string Name { get; set; }
 
-        public double Price { get; set; }
+        public double Price
+        {
+            get
+            {
+                return price;
+            }
+
+            set
+            {
+                if (value > 0)
+                {
+                    price = value;
+                }
+            }
+        }
 
         public void Present()
         {
